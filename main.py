7991c22a352d02d9ccf53b132220c59deeecc0a9
wangqiongle <1584787475@qq.com>
2023-11-30 21:56:54 +0800

    branch test

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.route('/')
-def test():
-    return {'hahahaha'}
+async def test():
+    return {'hello'}
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app='main', host='127.0.0.1',port=80,reload=True)
+    uvicorn.run(app='main:app', host='127.0.0.1',port=80,reload=True)
