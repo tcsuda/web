42493cbf7ad3dbeba5b81ac3650dda3b5cd2b714
wangqiongle <1584787475@qq.com>
2023-11-30 21:28:38 +0800

    fisrt api

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.route('/')
 def test():
     return {'hahahaha'}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app='main', host='127.0.0.1',port=80,reload=True)
